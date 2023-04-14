2d3d6776a55f648f14f9a9f16a93a988908e2dd8
AnkushRai-222 <ankushrai222@gmail.com>
2023-04-14 15:43:58 +0530

    Done :)

---
+++ JWT-Authentication/src/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const user = new mongoose.Schema({
+    firstName: String,
+    lastName:String,
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:String
+},{timestamps:true})
+
+module.exports = mongoose.model("User",user)
